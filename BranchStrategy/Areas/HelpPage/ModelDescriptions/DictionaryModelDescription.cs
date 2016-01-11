930c5021ebc1e0131c780f8c74a37ee03ad25945
Travis Gosselin <travis.gosselin@live.ca>
2016-01-11 13:34:04 -0500

    Initial commit of site.

---
+++ BranchStrategy/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BranchStrategy.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BranchStrategy/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BranchStrategy.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
